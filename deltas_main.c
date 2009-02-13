716eba6e727a82a7f6a5ac170fd76adb8ec93120
huttone <huttone@7cc61c5a-d85f-0410-b98b-f7f33de06df1>
2009-02-13 01:59:14 +0000

    * Use a minimal main to drive the model.

--- deltas_main.c
+++ deltas_main.c
@@ -1,33 +1,24 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <deltas_api.h>
 
 int
 main( void )
 {
-   Deltas_state* s_0 = deltas_new();
-   Deltas_state* s_1 = deltas_new();
+  Deltas_state* s = deltas_init( NULL );
 
-   deltas_init( s_0 );
-   deltas_init( s_1 );
+  deltas_set_save_file( s, "fileout_0" );
 
-   deltas_set_save_file( s_0, "fileout_0" );
-   deltas_set_save_file( s_1, "fileout_1" );
+  {
+    int i;
+    const double dt = 5.2;
 
-   {
-      int i;
-      for ( i=0; i<=100; i++ )
-      {
-         deltas_run_until( s_0, i*26.F );
-         deltas_run_until( s_1, i*52.F );
-      }
-   }
+    for ( i=0; i<=100; i++ )
+      deltas_run_until( s, i*dt );
+  }
 
-   deltas_finalize( s_0 );
-   deltas_finalize( s_1 );
+  deltas_finalize( s );
 
-   deltas_destroy( s_0 );
-   deltas_destroy( s_1 );
-
-   return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
 
