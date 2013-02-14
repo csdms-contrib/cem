e7b42740d308559cb96f4cb42a7936f6d9faf0f9
huttone <huttone@7cc61c5a-d85f-0410-b98b-f7f33de06df1>
2013-02-14 00:13:55 +0000

    * Fix pkgconfig files to properly set -l library flags.

--- waves.pc.cmake
+++ waves.pc.cmake
@@ -1,6 +1,6 @@
 Name: LibWaves
 Description: Waves library
 Version: ${WAVES_VERSION}
-Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lwaves
+Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lbmiwaves
 Cflags: -I${CMAKE_INSTALL_PREFIX}/include
 
