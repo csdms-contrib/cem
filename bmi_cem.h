459d1064f50b3f0a4b97091bcf08b9505f025cb3
mcflugen <mcflugen@gmail.com>
2015-04-21 16:13:05 -0600

    Added new-style BMI implementation files.

---
+++ bmi_cem.h
@@ -0,0 +1,16 @@
+#ifndef BMI_CEM_H_INCLUDED
+#define BMI_CEM_H_INCLUDED
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#include "bmi.h"
+
+BMI_Model * register_bmi_cem(BMI_Model *model);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif
