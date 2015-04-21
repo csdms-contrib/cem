f102ccae917f8903e7254c39ace07f6da563d9ca
mcflugen <mcflugen@gmail.com>
2015-04-21 16:21:39 -0600

    Changed to use new library name.

--- deltas.pc.cmake
+++ deltas.pc.cmake
@@ -1,6 +1,6 @@
 Name: LibWaves
 Description: Waves library
 Version: ${WAVES_VERSION}
-Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lbmicem
+Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lbmi_cem
 Cflags: -I${CMAKE_INSTALL_PREFIX}/include
 
--- waves.pc.cmake
+++ waves.pc.cmake
@@ -1,6 +1,6 @@
 Name: LibWaves
 Description: Waves library
 Version: ${WAVES_VERSION}
-Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lbmiwaves
+Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lbmi_waves
 Cflags: -I${CMAKE_INSTALL_PREFIX}/include
 
