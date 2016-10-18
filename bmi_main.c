01ea4a9af481752a88744aedf8b17bb3f1e51730
mcflugen <mcflugen@gmail.com>
2016-10-18 11:13:44 -0600

    Changed to use new function pointer BMI.

--- bmi_main.c
+++ bmi_main.c
@@ -8,22 +8,22 @@
 int
 main (void)
 {
-  BMI_CEM_Model *self = NULL;
+  BMI_Model *cem = (BMI_Model*)malloc(sizeof(BMI_Model));
   int err = BMI_SUCCESS;
+  int status;
 
-  {
-    fprintf (stdout, "Initializing... ");
+  register_bmi_cem(cem);
 
-    err = BMI_CEM_Initialize (NULL, &self);
-    if (err || !self)
-      return EXIT_FAILURE;
-
-    fprintf (stdout, "PASS\n");
+  status = cem->initialize(NULL, &cem);
+  if (status == BMI_FAILURE) {
+    fprintf (stdout, "FAIL.\n");
+    fprintf (stderr, "Unable to initialize\n");
+    return EXIT_FAILURE;
   }
 
   {
     char name[BMI_MAX_COMPONENT_NAME];
-    BMI_CEM_Get_component_name (self, name);
+    cem->get_component_name(cem->self, name);
     fprintf (stdout, "%s\n", name);
   }
 
@@ -35,7 +35,7 @@ main (void)
     for (i = 0; i < n_steps; i++)
     {
       fprintf (stdout, "Running until t = %d... ", i+1);
-      if (BMI_CEM_Update (self)==0 && BMI_CEM_Get_current_time (self, &time)==0) {
+      if (cem->update(cem->self)==0 && cem->get_current_time(cem->self, &time)==0) {
         if (fabs (time-(i+1)) < 1e-6)
           fprintf (stdout, "PASS\n");
         else {
@@ -47,7 +47,7 @@ main (void)
     }
 
     fprintf (stdout, "Running until t = %f... ", 1000.5);
-    if (BMI_CEM_Update_until (self, 1000.5)==0 && BMI_CEM_Get_current_time (self, &time)==0) {
+    if (cem->update_until(cem->self, 1000.5)==0 && cem->get_current_time (cem->self, &time)==0) {
         if (fabs (time-1000.5) < 1e-6)
           fprintf (stdout, "PASS\n");
         else {
@@ -60,7 +60,7 @@ main (void)
   }
 
   fprintf (stdout, "Finalizing... ");
-  err = BMI_CEM_Finalize (self);
+  err = cem->finalize (cem->self);
   if (err)
     return EXIT_FAILURE;
   fprintf (stdout, "PASS\n");
