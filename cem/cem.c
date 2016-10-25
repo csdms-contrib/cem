ca3cc9c7d3e510444ce59c5307a342107c0f2225
mcflugen <mcflugen@gmail.com>
2016-10-25 13:40:14 -0600

    Remove temporary fixes to InitNormal.
    I've removed a couple temporary fixes that should be taken care
    of in a pull request that fixes issue #6.

--- cem/cem.c
+++ cem/cem.c
@@ -3619,15 +3619,6 @@ void FixBeach(void)
       else
         y = 2 * Y_MAX - i;
 
-      if (y <= 0) {
-        // fprintf(stderr, "There is a chance for an underwrite (%d)\n", y);
-        continue;
-      }
-      if (y >= 2 * Y_MAX - 1) {
-        // fprintf(stderr, "There is a chance for an overwrite (%d)\n", y);
-        continue;
-      }
-
       /*Take care of corner problem?
          if  (((AllBeach[x][y] == 'n') && (PercentFullRock[x][y] > 0.0)) &&
          (((AllBeach[x][y-1] == 'n') && (AllBeach[x-1][y] == 'n') &&
@@ -4069,12 +4060,6 @@ void InitNormal(void)
   for (x = 0; x < X_MAX; x++) {
     for (n = 0; n <= 2 * NUMBER_CHUNK; n++) {
       for (y = n * CHUNK_LENGTH; y < ((n + 2) * CHUNK_LENGTH); y++) {
-
-        if (y >= 2 * Y_MAX) {
-          // fprintf(stderr, "y is too big. this needs fixing (%d).\n", y);
-          continue;
-        }
-
         if (n % 3 == 0 && (!blocks || x > InitialRock - 3)) { /* if even */
           TypeOfRock[x][y] = 's';
           topography[x][y] = kCliffHeightSlow;
